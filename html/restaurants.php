b766acab8ccf463c1a0822ec8773b755ee159bca
mateevd <mateevd@gmail.com>
2017-09-13 14:42:45 +0200

    add google maps to restaurants

--- html/restaurants.php
+++ html/restaurants.php
@@ -6,14 +6,85 @@
 <body>
 
 <!-- Navigation bar -->
+
 <?php include "../_includes/navigation.php" ?>
 
+<!-- restorants locations -->
 
+<div class="container">
+
+    <div class="row">
+        <div class="col-md-8">
+            <h2>Nos Restaurants</h2>
+
+            <iframe class="googmap" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1414.70336915776!2d-0.5671161240249523!3d44.83364949685665!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xd5527cba74d1bdf%3A0xb81d5dd07bca4e2e!2s15+Place+Canteloup%2C+33800+Bordeaux!5e0!3m2!1sen!2sfr!4v1505305249997" width="600" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>
+
+        </div>
+        <br>
+        <br>
+        <div class="col-md-4">
+            <h2>Adress</h2>
+            <p></p>
+
+            <h2>Horaires</h2>
+            <p></p>
+
+            <h2>Acces</h2>
+            <p></p>
+
+
+        </div>
+    </div>
+    <hr>
+    <div class="row">
+        <div class="col-md-8">
+
+
+            <iframe class="googmap" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2645.1259004399103!2d1.0096356511168192!3d48.47329863559584!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47e3c1f7f154cb2f%3A0x56c92fac27b1deca!2s18+Rue+de+la+Gare%2C+28240+La+Loupe!5e0!3m2!1sen!2sfr!4v1505306413054" width="600" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>
+
+        </div>
+        <div class="col-md-4">
+            <h2>Adress</h2>
+            <p></p>
+
+            <h2>Horaires</h2>
+            <p></p>
+
+            <h2>Acces</h2>
+            <p></p>
+
+
+        </div>
+    </div>
+    <hr>
+    <div class="row">
+        <div class="col-md-8">
+
+
+            <iframe class="googmap" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2530.69479675598!2d3.0155578511877996!3d50.63278628199037!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47c2d55ba8f0e18f%3A0x170361d02ab8fa98!2s4+Avenue+des+Saules%2C+59160+Lille!5e0!3m2!1sen!2sfr!4v1505306477588" width="600" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>
+
+        </div>
+        <div class="col-md-4">
+            <h2>Adress</h2>
+            <p></p>
+
+            <h2>Horaires</h2>
+            <p></p>
+
+            <h2>Acces</h2>
+            <p></p>
+
+
+        </div>
+    </div>
+</div>
 
 
 <script src="https://use.fontawesome.com/2e8d9444e4.js"></script>
 <script src="https://code.jquery.com/jquery-3.2.1.min.js" integrity="sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4=" crossorigin="anonymous"></script>
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBu5nZKbeK-WHQ70oqOWo-_4VmwOwKP9YQ"></script>
+
 </body>
 <footer>
     <?php include "../_includes/footer.php" ?>
