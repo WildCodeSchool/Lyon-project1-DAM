563cbac02b71021ae5e2f6a3bd54a293c9c7666c
mateevd <mateevd@gmail.com>
2017-09-14 18:41:06 +0200

    changes to construction

--- html/construction.php
+++ html/construction.php
@@ -25,7 +25,7 @@
     <hr>
 
     <!-- Alert message-->
-    <img class="img" src="https://cdn.pixabay.com/photo/2017/06/16/07/26/under-construction-2408061_960_720.png" style="margin-top: auto" alt="under">
+    <img class="img" src="http://www.manusamoa.com/images/Under-construction.png" style="margin-top: auto" alt="under">
 </div>
 
 <!-- Scripts -->
