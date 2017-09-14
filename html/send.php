646f9253e89d1be2c559ce7d2b393e4a97cb203e
mateevd <mateevd@gmail.com>
2017-09-14 12:09:00 +0200

    add send page + changes to contact page

---
+++ html/send.php
@@ -0,0 +1,37 @@
+<!-- <?php
+
+header("refresh:5; url= index.php");
+exit();
+
+?> -->
+
+<!DOCTYPE html>
+<html lang="en">
+
+<?php include "../_includes/head.php" ?>
+
+<body>
+
+<?php include "../_includes/navigation.php" ?>
+
+
+<!-- Main container -->
+
+<div class="container">
+    <div class="alert alert-success" role="alert">Your message has been send! You'be redirected to our home page</div>
+</div>
+
+
+
+<!-- JS scripts -->
+
+<script src="https://use.fontawesome.com/2e8d9444e4.js"></script>
+<script src="https://code.jquery.com/jquery-3.2.1.min.js" integrity="sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4=" crossorigin="anonymous"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+
+
+</body>
+<footer>
+    <?php include "../_includes/footer.php" ?>
+</footer>
+</html>
