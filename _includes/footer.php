a36cfc01066b3af3be799add65ed4f49574afd3e
mateevd <mateevd@gmail.com>
2017-09-15 17:10:55 +0200

    fixing footer include + style (not finished)

--- _includes/footer.php
+++ _includes/footer.php
@@ -1,23 +1,25 @@
-<div class="row row-centered margT-20">
-    <div class="col-lg-4 col-md-4 col-sm-8 col-xs-10 col-centered bord-top"></div>
-</div>
-<div class="row row-centered">
-    <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 col-centered">
-        <div class="text-center infosEcopy">
-            <ul class="text-center">
-                <li><a href="carte.php">Nos Burgers</a></li>
-                <li><a href="history.php">Notre équipe</a></li>
-                <li><a href="restaurants.php">Nos Restaurants</a></li>
-                <li><a href="FAQ.php">FAQ</a></li>
-            </ul>
-            <p class="text-muted">
-                Pour votre santé, mangez au moins cinq fruits et légumes par jour. <a target="_blank"
-                                                                                      href="http://www.mangerbouger.fr/"><b
-                            class="last-b">www.mangerbouger.fr</b></a>.
-                <br>
-                <a href="mentions legales.php"><b>Mentions Légales</b> </a> <a href=""><b class="last-b">Contact</b></a>
-                <br>Développement web : <a target="_blank" href="http://www.wildcodeschool.fr/"><b class="last-b">www.wildcodeschool.fr/</b></a>
-            </p>
+<div class="container-fluid"> <!--container-->
+
+    <div class="row">
+        <div class="col-md-6 col-md-push-4">
+
+        <span class="text-muted">Pour votre santé, mangez au moins cinq fruits et légumes par jour.
+            <a target="_blank" href="http://www.mangerbouger.fr/" /> www.mangerbouger.fr</span>
+        <a href="../html/mention legales.php"> <b>Mentions Légales</b> </a> <br>
+        Développement web : <a target="_blank" href="http://www.wildcodeschool.fr/">www.wildcodeschool.fr</a>
+        </div>
+
+        <div class="col-md-6 col-md-pull-7">
+
+            <a href="../html/carte.php">Nos Burgers</a>
+            <br>
+            <a href="../html/history.php">Notre équipe</a>
+            <br>
+            <a href="../html/restaurants.php">Nos Restaurants</a>
+            <br>
+            <a href="../html/FAQ.php">FAQ</a>
+            <br>
+
         </div>
     </div>
 </div>
