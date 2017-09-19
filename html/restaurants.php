40876bd6fd566ae3fc7dabf37033237d10d031e9
mateevd <mateevd@gmail.com>
2017-09-19 17:49:50 +0200

    working on page restorants.php

--- html/restaurants.php
+++ html/restaurants.php
@@ -15,14 +15,16 @@
     <!-- header -->
     <?php include "../_includes/header.php" ?>
 
-<!--Resto Bordeaux -->
+    <!--Resto Bordeaux -->
     <h1>Nos Restaurants :<br> <br></h1>
 
     <div class="row">
         <div class="col-md-8">
 
 
-            <iframe class="googmap" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1414.70336915776!2d-0.5671161240249523!3d44.83364949685665!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xd5527cba74d1bdf%3A0xb81d5dd07bca4e2e!2s15+Place+Canteloup%2C+33800+Bordeaux!5e0!3m2!1sen!2sfr!4v1505305249997" width="600" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>
+            <iframe class="googmap"
+                    src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1414.70336915776!2d-0.5671161240249523!3d44.83364949685665!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xd5527cba74d1bdf%3A0xb81d5dd07bca4e2e!2s15+Place+Canteloup%2C+33800+Bordeaux!5e0!3m2!1sen!2sfr!4v1505305249997"
+                    width="500" height="350" frameborder="0" style="border:0" allowfullscreen></iframe>
 
         </div>
         <div class="adresse col-md-4">
@@ -44,8 +46,9 @@
     <div class="row">
         <div class="col-md-8">
 
-
-            <iframe class="googmap" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2645.1259004399103!2d1.0096356511168192!3d48.47329863559584!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47e3c1f7f154cb2f%3A0x56c92fac27b1deca!2s18+Rue+de+la+Gare%2C+28240+La+Loupe!5e0!3m2!1sen!2sfr!4v1505306413054" width="600" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>
+            <iframe class="googmap"
+                    src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2645.1259004399103!2d1.0096356511168192!3d48.47329863559584!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47e3c1f7f154cb2f%3A0x56c92fac27b1deca!2s18+Rue+de+la+Gare%2C+28240+La+Loupe!5e0!3m2!1sen!2sfr!4v1505306413054"
+                    width="600" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>
 
         </div>
         <div class="adresse col-md-4">
@@ -64,12 +67,12 @@
 
     <!--Resto Lille -->
 
-
     <div class="row">
         <div class="col-md-8">
 
-
-            <iframe class="googmap" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2530.69479675598!2d3.0155578511877996!3d50.63278628199037!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47c2d55ba8f0e18f%3A0x170361d02ab8fa98!2s4+Avenue+des+Saules%2C+59160+Lille!5e0!3m2!1sen!2sfr!4v1505306477588" width="600" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>
+            <iframe class="googmap"
+                    src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2530.69479675598!2d3.0155578511877996!3d50.63278628199037!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47c2d55ba8f0e18f%3A0x170361d02ab8fa98!2s4+Avenue+des+Saules%2C+59160+Lille!5e0!3m2!1sen!2sfr!4v1505306477588"
+                    width="600" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>
 
         </div>
         <div class="adresse col-md-4">
@@ -81,7 +84,6 @@
             <h2>Horaires</h2>
             <h4>Restaurant ouvert 7j/7 <br> <br>
                 12h – 14h / 19h – 22h</h4>
-
         </div>
     </div>
     <hr>
@@ -91,8 +93,9 @@
     <div class="row">
         <div class="col-md-8">
 
-
-            <iframe class="googmap" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2784.2426722067603!2d4.824636651031088!3d45.746280722495335!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47f4ea4ab604554f%3A0x83e3e33b33887834!2s17+Rue+Delandine%2C+69002+Lyon!5e0!3m2!1sen!2sfr!4v1505307622172" width="600" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>
+            <iframe class="googmap"
+                    src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2784.2426722067603!2d4.824636651031088!3d45.746280722495335!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47f4ea4ab604554f%3A0x83e3e33b33887834!2s17+Rue+Delandine%2C+69002+Lyon!5e0!3m2!1sen!2sfr!4v1505307622172"
+                    width="600" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>
 
         </div>
         <div class="adresse col-md-4">
@@ -115,7 +118,9 @@
         <div class="col-md-8">
 
 
-            <iframe class="googmap" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2675.1915730920136!2d1.891984151098238!3d47.89397727597285!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47e4e52661ecb4eb%3A0x86bbb8261faea07!2s1+Avenue+du+Champ+de+Mars%2C+45100+Orl%C3%A9ans!5e0!3m2!1sen!2sfr!4v1505307804449" width="600" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>
+            <iframe class="googmap"
+                    src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2675.1915730920136!2d1.891984151098238!3d47.89397727597285!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47e4e52661ecb4eb%3A0x86bbb8261faea07!2s1+Avenue+du+Champ+de+Mars%2C+45100+Orl%C3%A9ans!5e0!3m2!1sen!2sfr!4v1505307804449"
+                    width="600" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>
 
         </div>
         <div class="adresse col-md-4">
@@ -137,7 +142,9 @@
         <div class="col-md-8">
 
 
-            <iframe class="googmap" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2625.4789962838922!2d2.3504061511289738!3d48.849075809215016!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47e671e4fa5fdbf9%3A0x5dcb5a09cff26472!2s11+Rue+de+Poissy%2C+75005+Paris!5e0!3m2!1sen!2sfr!4v1505307876833" width="600" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>
+            <iframe class="googmap"
+                    src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2625.4789962838922!2d2.3504061511289738!3d48.849075809215016!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47e671e4fa5fdbf9%3A0x5dcb5a09cff26472!2s11+Rue+de+Poissy%2C+75005+Paris!5e0!3m2!1sen!2sfr!4v1505307876833"
+                    width="600" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>
 
         </div>
         <div class="adresse col-md-4">
@@ -160,7 +167,9 @@
         <div class="col-md-8">
 
 
-            <iframe class="googmap" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2889.241023090167!2d1.4398347509667566!3d43.601522963686485!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x12aebb62f2131421%3A0xfca615e480acd9f4!2s1+Place+de+la+Bourse%2C+31000+Toulouse!5e0!3m2!1sen!2sfr!4v1505307924205" width="600" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>
+            <iframe class="googmap"
+                    src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2889.241023090167!2d1.4398347509667566!3d43.601522963686485!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x12aebb62f2131421%3A0xfca615e480acd9f4!2s1+Place+de+la+Bourse%2C+31000+Toulouse!5e0!3m2!1sen!2sfr!4v1505307924205"
+                    width="600" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>
 
         </div>
         <div class="adresse col-md-4">
@@ -181,8 +190,11 @@
 
 
 <script src="https://use.fontawesome.com/2e8d9444e4.js"></script>
-<script src="https://code.jquery.com/jquery-3.2.1.min.js" integrity="sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4=" crossorigin="anonymous"></script>
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+<script src="https://code.jquery.com/jquery-3.2.1.min.js"
+        integrity="sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4=" crossorigin="anonymous"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"
+        integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa"
+        crossorigin="anonymous"></script>
 <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBu5nZKbeK-WHQ70oqOWo-_4VmwOwKP9YQ"></script>
 
 <footer>
