14699635838307c562160f1d3abe2854914255a2
AR6A <raza.andy@gmail.com>
2017-09-19 10:19:34 +0200

    update files

--- _includes/footer.php
+++ _includes/footer.php
@@ -3,7 +3,7 @@
     <div class="row">
         <div class="col-md-6 col-md-push-4">
 
-        <span class="text-muted">Pour votre santé, mangez au moins cinq fruits et légumes par jour.
+        <span>Pour votre santé, mangez au moins cinq fruits et légumes par jour.
             <a target="_blank" href="http://www.mangerbouger.fr/" /> www.mangerbouger.fr</span>
         <a href="../html/mention legales.php"> <b>Mentions Légales</b> </a> <br>
         Développement web : <a target="_blank" href="http://www.wildcodeschool.fr/">www.wildcodeschool.fr</a>
