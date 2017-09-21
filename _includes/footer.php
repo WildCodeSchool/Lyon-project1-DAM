410ffafc4d15894fa83ad9086d54e71987e1bb3d
Manuel Briand <manuel.briand354@gmail.com>
2017-09-21 16:59:07 +0200

    fix FAQ, add css footer

--- _includes/footer.php
+++ _includes/footer.php
@@ -1,7 +1,7 @@
 <div class="container-fluid"> <!--container-->
 
     <div class="row">
-        <div class="col-md-6 col-md-push-4">
+        <div class="foot col-md-6 col-md-push-4">
 
         <span>Pour votre santé, mangez au moins cinq fruits et légumes par jour.
             <a id="" target="_blank" href="http://www.mangerbouger.fr/" /> www.mangerbouger.fr</span>
@@ -9,7 +9,7 @@
         Développement web : <a target="_blank" href="http://www.wildcodeschool.fr/">www.wildcodeschool.fr</a>
         </div>
 
-        <div class="col-md-6 col-md-pull-7">
+        <div class="foot col-md-6 col-md-pull-7">
 
             <a href="../html/carte.php">Nos Burgers</a>
             <br>
--- html/FAQ.php
+++ html/FAQ.php
@@ -9,91 +9,126 @@
 
 <?php include "../_includes/navigation.php" ?>
 
+<!-- header -->
+<?php include "../_includes/header.php" ?>
+
+
 <!-- FAQ -->
 
+<div class="container">
 
-<div class="col-xs-12 col-md-10">
-    <h1>
-        <strong>Les questions que l'on peut se poser avant de manger des insectes</strong>
-    </h1>
 
-    <h2 id="faq_1"><strong>Pourquoi manger des insectes ?</strong>
-    </h2>
+    <div class="row"> <!-- First row -->
+        <div class="equipe col-xs-12 col-md-10">
+            <h1>
+                <strong>Les questions que l'on peut se poser avant de manger des insectes</strong>
+            </h1>
 
-    <p>
-        Manger des insectes est-ce si bizarre ? Manger des insectes n'est pas plus bizarre que de manger des escargots
-        ou
-        de la viande ! Saviez-vous qu’environ 2,5 milliards de personnes mangent régulièrement des insectes dans le
-        monde. Très consommés en Asie, Afrique et en Amérique latine, les insectes comestibles reviennent depuis
-        plusieurs années sur le devant de la scène en Europe.<br>Les insectes sont une vraie mine d’or nutritionnelle :
-        protéines de bonne qualité, nutriments, vitamines…tout est bon dans l’insecte ! sans compter un impact
-        écologique quasi nul comparé à un élevage de bœuf, de porc ou de poulet. Les rejets sur certains gaz à effets de
-        serre entre un élevage de boeuf et un élevage d'insectes par exemple&nbsp;diminuent de 95% ! Alors pourquoi ne
-        pas, vous aussi, vous initier et manger des insectes pour la planète, pour votre plaisir ou pour la fantaisie!
-    </p>
+            <h2 id="faq_1"><strong>Pourquoi manger des insectes ?</strong>
+            </h2>
 
-    <h2 id="faq_2">
-        <strong>Quel est le goût des insectes comestibles ?</strong>
-    </h2>
+            <p>
+                Manger des insectes est-ce si bizarre ? Manger des insectes n'est pas plus bizarre que de manger des
+                escargots
+                ou
+                de la viande ! Saviez-vous qu’environ 2,5 milliards de personnes mangent régulièrement des insectes dans
+                le
+                monde. Très consommés en Asie, Afrique et en Amérique latine, les insectes comestibles reviennent depuis
+                plusieurs années sur le devant de la scène en Europe.<br>Les insectes sont une vraie mine d’or
+                nutritionnelle :
+                protéines de bonne qualité, nutriments, vitamines…tout est bon dans l’insecte ! sans compter un impact
+                écologique quasi nul comparé à un élevage de bœuf, de porc ou de poulet. Les rejets sur certains gaz à
+                effets de
+                serre entre un élevage de boeuf et un élevage d'insectes par exemple&nbsp;diminuent de 95% ! Alors
+                pourquoi ne
+                pas, vous aussi, vous initier et manger des insectes pour la planète, pour votre plaisir ou pour la
+                fantaisie!
+            </p>
 
-    <p>
-        Il existe plus de 2000 espèces d’insectes comestibles dans le monde. Chacune a son goût ! Certaines espèces ont
-        un goût similaire à quelque chose que vous connaissez déjà mais l’autre majorité des insectes comestibles a un
-        goût que vous ne pourrez comparer à rien d’autres ! Chaque espèce d’insectes comestibles a sa personnalité, son
-        goût.&nbsp;<br>Insectescomestibles.fr propose des animations et dégustations autour des insectes comestibles&nbsp;pour
-        des soirées privées, événements d’entreprises ou salons. Souvent lors de ces animations, les personnes nous
-        demandent quel est le goût de ces insectes avant de passer le cap et de manger leur premier insectes. Nous
-        parvenons pour certaines espèces à faire des rapprochements : par exemple, les grillons et les criquets ont un
-        goût légèrement amande, les vers de farine ont un goût s’apparentant à des notes de noisettes tandis que les
-        termites rouges ressemblent à du bacon !&nbsp;<br>Il faudra trouver dégustation après dégustation, vos espèces
-        et goûts préférés ! Ne soyez donc pas étonné si en goûtant certains insectes comestibles vous ne puissiez pas
-        réellement décrire le goût ! C’est même là l’un des aspects les plus sympa, s’éveiller à d’autres goûts et
-        d’autres cultures.
-    </p>
+            <h2 id="faq_2">
+                <strong>Quel est le goût des insectes comestibles ?</strong>
+            </h2>
 
-    <h2 id="faq_3">
-        <strong>Est-ce que les insectes comestibles sont caloriques ?</strong>
-    </h2>
+            <p>
+                Il existe plus de 2000 espèces d’insectes comestibles dans le monde. Chacune a son goût ! Certaines
+                espèces ont
+                un goût similaire à quelque chose que vous connaissez déjà mais l’autre majorité des insectes
+                comestibles a un
+                goût que vous ne pourrez comparer à rien d’autres ! Chaque espèce d’insectes comestibles a sa
+                personnalité, son
+                goût.&nbsp;<br>Insectescomestibles.fr propose des animations et dégustations autour des insectes
+                comestibles&nbsp;pour
+                des soirées privées, événements d’entreprises ou salons. Souvent lors de ces animations, les personnes
+                nous
+                demandent quel est le goût de ces insectes avant de passer le cap et de manger leur premier insectes.
+                Nous
+                parvenons pour certaines espèces à faire des rapprochements : par exemple, les grillons et les criquets
+                ont un
+                goût légèrement amande, les vers de farine ont un goût s’apparentant à des notes de noisettes tandis que
+                les
+                termites rouges ressemblent à du bacon !&nbsp;<br>Il faudra trouver dégustation après dégustation, vos
+                espèces
+                et goûts préférés ! Ne soyez donc pas étonné si en goûtant certains insectes comestibles vous ne
+                puissiez pas
+                réellement décrire le goût ! C’est même là l’un des aspects les plus sympa, s’éveiller à d’autres goûts
+                et
+                d’autres cultures.
+            </p>
 
-    <p>
-        Les insectes sont particulièrement indiqués dans le cadre d’un régime ! Aucune crainte : très peu calorique,
-        emplis de bons nutriments, vitamines et protéines de qualité, ils sont même un bon allié minceur. Composé à 72%
-        de protéines, seulement 16% de graisses et 12% de glucides avec 96Kcal par portion, vous auriez tort de vous en
-        priver!
-    </p>
+            <h2 id="faq_3">
+                <strong>Est-ce que les insectes comestibles sont caloriques ?</strong>
+            </h2>
 
-    <h2 id="faq_4">
-        <strong>Peut-on faire une allergie alimentaire aux insectes ?</strong>
-    </h2>
+            <p>
+                Les insectes sont particulièrement indiqués dans le cadre d’un régime ! Aucune crainte : très peu
+                calorique,
+                emplis de bons nutriments, vitamines et protéines de qualité, ils sont même un bon allié minceur.
+                Composé à 72%
+                de protéines, seulement 16% de graisses et 12% de glucides avec 96Kcal par portion, vous auriez tort de
+                vous en
+                priver!
+            </p>
 
-    <p>
-        Sur chaque fiche produit de notre site et aussi sur chaque packaging de nos produits, vous trouverez les
-        recommandations et informations spécifiques à la consommation d'insectes. Il peut exister des allergies aux
-        insectes comme à tout produit alimentaire, nous vous conseillons plus particulièrement d’être vigilants en cas
-        de terrain allergique (aux crustacés, acariens...).&nbsp;
-    </p>
+            <h2 id="faq_4">
+                <strong>Peut-on faire une allergie alimentaire aux insectes ?</strong>
+            </h2>
 
-    <h2 id="faq_5">
-        <strong>Est-ce que les enfants peuvent manger des insectes ?&nbsp;</strong>
-    </h2>
+            <p>
+                Sur chaque fiche produit de notre site et aussi sur chaque packaging de nos produits, vous trouverez les
+                recommandations et informations spécifiques à la consommation d'insectes. Il peut exister des allergies
+                aux
+                insectes comme à tout produit alimentaire, nous vous conseillons plus particulièrement d’être vigilants
+                en cas
+                de terrain allergique (aux crustacés, acariens...).&nbsp;
+            </p>
 
-    <p>
-        Les insectes comestibles sont déconseillés aux enfants de moins de 3 ans, les insectes comestibles sont
-        équivalents à une cacahuète : les enfants peuvent très facilement s'étouffer en oubliant de mâcher. Après cet
-        âge de 3 ans et comme tout aliment, nous vous conseillons la plus grande prudence, si vous souhaitez faire
-        goûter nos insectes à vos enfants.
-    </p>
+            <h2 id="faq_5">
+                <strong>Est-ce que les enfants peuvent manger des insectes ?&nbsp;</strong>
+            </h2>
 
+            <p>
+                Les insectes comestibles sont déconseillés aux enfants de moins de 3 ans, les insectes comestibles sont
+                équivalents à une cacahuète : les enfants peuvent très facilement s'étouffer en oubliant de mâcher.
+                Après cet
+                âge de 3 ans et comme tout aliment, nous vous conseillons la plus grande prudence, si vous souhaitez
+                faire
+                goûter nos insectes à vos enfants.
+            </p>
+
+        </div>
+    </div>
 </div>
-
 <!-- JS scripts -->
 
 <script src="https://use.fontawesome.com/2e8d9444e4.js"></script>
-<script src="https://code.jquery.com/jquery-3.2.1.min.js" integrity="sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4=" crossorigin="anonymous"></script>
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+<script src="https://code.jquery.com/jquery-3.2.1.min.js"
+        integrity="sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4=" crossorigin="anonymous"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"
+        integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa"
+        crossorigin="anonymous"></script>
 
-</body>
 <footer>
     <?php include "../_includes/footer.php" ?>
 </footer>
+</body>
 </html>
