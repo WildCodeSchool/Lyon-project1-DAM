7cc214f65351452ae81e4ea1731a3dbf32ec0d28
Manuel Briand <manuel.briand354@gmail.com>
2017-09-22 09:06:56 +0200

    add color link

--- _includes/footer.php
+++ _includes/footer.php
@@ -1,10 +1,10 @@
-<div class="container-fluid"> <!--container-->
+<div class="container-fluid" xmlns="http://www.w3.org/1999/html"> <!--container-->
 
     <div class="row">
         <div class="foot col-md-6 col-md-push-4">
 
         <span>Pour votre santé, mangez au moins cinq fruits et légumes par jour.
-            <a id="" target="_blank" href="http://www.mangerbouger.fr/" /> www.mangerbouger.fr</span>
+            <a id="" target="_blank" href="http://www.mangerbouger.fr/" > www.mangerbouger.fr</a> </span>
         <a href="../html/mention legales.php"> <b>Mentions Légales</b> </a> <br>
         Développement web : <a target="_blank" href="http://www.wildcodeschool.fr/">www.wildcodeschool.fr</a>
         </div>
